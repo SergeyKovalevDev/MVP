e376b01e1dfd2a4c01a12b336cfcd8208363656f
Sergey Kovalev <dip.deep@mail.ru>
2022-03-05 19:19:24 +0300

    initial commit

---
+++ src/test/java/com/dontexist/MVP1/Mvp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontexist.MVP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
